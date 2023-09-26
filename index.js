fcfa5baa58b6600440967c47e1d273c35a0d9a8c
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-09-25 20:08:00 -0400

    push all

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
 
-const app = express();
 
+const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
+// setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -17,15 +17,16 @@ app.get('/api/blocks', (req, res) => {
 });
 
 app.post('/api/mine', (req,res) => {
-    const {data} = req.body;
-
+    const { data } = req.body;
     blockchain.addBlock({data});
-
+    pubsub.broadcastChain()
     res.redirect('/api/blocks');
 });
 
+
 const DEFAULT_PORT = 3000;
-let PEER_PORT; 
+let PEER_PORT;
+
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000)
@@ -33,5 +34,4 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
-
 
--- pubsub.js
+++ pubsub.js
@@ -13,16 +13,17 @@ class PubSub {
 
     this.publisher = redis.createClient();
     this.subscriber = redis.createClient();
+    
 
-    this.subscriber.subscribe(CHANNELS.TEST);
+    this.subscribeToChannels()
 
     this.subscriber.on(
       'message', 
-      (channel, message) => this.handleMEssage(channel, message)
+      (channel, message) => this.handleMessage(channel, message)
     );
   }
 
-  handleMEssage(channel, message) {
+  handleMessage(channel, message) {
     console.log(`Message received. Channel: ${channel}. Message ${message}`);
     
     const parsedMessage = JSON.parse(message);
@@ -39,7 +40,10 @@ class PubSub {
   }
 
   publish({ channel, message}) {
-    this.publisher.publish(channel, message);
+    this.publisher.publish(channel, message, () => {
+       this.subscriber.subscribe(channel);
+    });
+
   }
 
   broadcastChain() {
@@ -50,132 +54,4 @@ class PubSub {
   }
 }
 
-const testPubSub = new PubSub();
-
-testPubSub.publisher.publish(CHANNELS.TEST, 'foo');
 module.exports = PubSub;
-
-// const redis = require('redis');
-
-// const CHANNELS = {
-//   TEST: 'TEST', 
-//   BLOCKCHAIN: 'BLOCKCHAIN'
-// };
-
-// class PubSub {
-//   constructor(blockchain) {
-//     this.blockchain = blockchain;
-
-//     this.publisher = redis.createClient();
-//     this.subscriber = redis.createClient();
-
-//     this.subscribeToChannels();
-
-//   }
-
-//   async init() {
-//     await this.publisher.connect();
-//     await this.subscriber.connect();
-//   }
-
-//   subscribeToChannels() {
-//     Object.values(CHANNELS).forEach(channel => {
-//       this.subscriber.subscribe(channel, (message, channel) => {
-//           console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
-    
-//           const parserMessage = JSON.parse(message);
-    
-//           if (channel === CHANNELS.BLOCKCHAIN) {
-//             console.log(`Message was for channel ${CHANNELS.BLOCKCHAIN}`);
-//           }
-//       });
-//     });
-//   }
-
-//   publish({ channel, message}){
-//     this.publisher.publish(channel, message);
-//   }
-
-//   broadcastChain() {
-//     this.publish({
-//         channel: CHANNELS.BLOCKCHAIN,
-//         message: JSON.stringify(this.blockchain.chain)
-//     });
-//   }
-// }
-
-// module.exports = PubSub;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const redis = require('redis');
-
-// const CHANNELS = {
-//   TEST: 'TEST', 
-//   BLOCKCHAIN: 'BLOCKCHAIN'
-// };
-
-// class PubSub {
-//   constructor(blockchain) {
-//     this.blockchain = blockchain;
-
-//     this.publisher = redis.createClient();
-//     this.subscriber = redis.createClient();
-
-//     this.subscribeToChannels();
-
-//   }
-
-//   // Changed signature here, not sure what message and channel were doing here?
-//   //async init({channel, message}) {
-//   async init() {
-//     await this.publisher.connect();
-//     await this.subscriber.connect();
-//   }
-
-//   subscribeToChannels() {
-//     Object.values(CHANNELS).forEach(channel => {
-//       // Added debug...
-//       // console.log(`Subscribing to ${channel}`);
-//       // Subscribe passing in the subscriber function... using
-//       // same function for all channels here.
-//       this.subscriber.subscribe(channel, (message, channel) => {
-//           console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
-    
-//           // Do something with this according to your application logic...
-//           const parserMessage = JSON.parse(message);
-    
-//           if (channel === CHANNELS.BLOCKCHAIN) {
-//             console.log(`Message was for channel ${CHANNELS.BLOCKCHAIN}`);
-//           }
-//       });
-//     });
-//   }
-
-//   publish({ channel, message}){
-//     this.publisher.publish(channel, message);
-//   }
-
-//   // I didn't use this as I don't know what this.blockchain.chain is.
-//   broadcastChain() {
-//     this.publish({
-//         channel: CHANNELS.BLOCKCHAIN,
-//         message: JSON.stringify(this.blockchain.chain)
-//     });
-//   }
-// }
-
-// const testPubSub = new PubSub();
-// testPubSub.init();
-// module.exports = PubSub;
-

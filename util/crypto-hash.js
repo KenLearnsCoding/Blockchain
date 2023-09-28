0eb9993470b81a28065015d8918fe35ffe6f230f
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-09-28 15:40:03 -0400

    push up

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -1,10 +1,11 @@
 const crypto = require('crypto');
+const { json } = require('express');
 
 
 const cryptoHash = (...inputs) => {
     const hash = crypto.createHash('sha256');
 
-    hash.update(inputs.sort().join(' '));
+    hash.update(inputs.map( input => JSON.stringify(input)).sort().join(' '));
 
     //the digest is a term in cryptography to represent the result of a hash.
 

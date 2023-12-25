49f009d17629d3f7084599c7b119b8fc379a2b27
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-12-24 23:12:49 -0500

    push

--- app/pubsub.js
+++ app/pubsub.js
@@ -1,4 +1,4 @@
-const redis = require('redis');
+ const redis = require('redis');
 
 const CHANNELS = {
   TEST: 'TEST',
@@ -30,7 +30,7 @@ class PubSub {
 
     switch(channel) {
       case CHANNELS.BLOCKCHAIN:
-        this.blockchain.replaceChain(parsedMessage, () => {
+        this.blockchain.replaceChain(parsedMessage, true, () => {
           this.transactionPool.clearBlockchainTransactions({
              chain: parsedMessage
           });

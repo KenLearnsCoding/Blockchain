d3cadeaf0decb1bc00c2a9fc7ccad617ad595343
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-12-28 03:33:21 -0500

    push

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
+// Importing the React library and the createRoot function from react-dom
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
 
-render(
-    <div>Cryptochain in React!: fifth Live update</div>, 
-    document.getElementById('root')
+// Using createRoot to create a root for rendering, passing the target root element
+const root = createRoot(document.getElementById('root'));
+
+// Rendering a React component (JSX syntax) inside the root using the render method
+root.render(
+  <App />,
 );
